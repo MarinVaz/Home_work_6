fe581456ee5cd2eaf8b0be9f96877763f1e3226d
Marina <errorda@gmail.com>
2022-11-29 16:18:17 +0300

    added task 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,36 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
+//которая покажет количество чётных чисел в массиве.
+
+Console.WriteLine("Введите размер массива:  ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int [size];
+int k=0;
+
+FillArrayRandomNumbers(numbers,100,1000);
+WriteArray(numbers);
+
+for (int i = 0; i< numbers.Length; i++)
+    {
+        if (numbers[i] % 2 == 0)
+        {k++;}
+                  
+    }
+ Console.Write($"Количество четных чисел {k}");
+
+void FillArrayRandomNumbers (int[] array, int min, int max)
+{
+    for (int i = 0; i< array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max+1);
+    }
+    }
+
+void WriteArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+       Console.Write(array[i]+" ");
+    }
+    Console.WriteLine();
+    }
+
